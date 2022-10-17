52c55c20ffbf6d1460eca96ae71587ed2039f073
Pablo Pirata <pablo@studocu.com>
2022-10-17 22:20:40 +0200

    feat: add phaser root to the index.html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { AUTO, Game } from 'phaser';
 import logo from './assets/react-logo.svg';
-import { GAME_CONTENT_ID } from './utils/constants';
 import MainScene from './game-scenes/MainScene';
 import './App.css';
 
@@ -10,7 +9,7 @@ function App() {
     if (!window.phaserGame) {
       const config = {
         type: AUTO,
-        parent: GAME_CONTENT_ID,
+        parent: 'game-content',
         width: 800,
         height: 600,
         scene: MainScene
@@ -24,9 +23,6 @@ function App() {
     <div className="App">
       <main className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div id={GAME_CONTENT_ID} key={GAME_CONTENT_ID}>
-          {/* this is where the game canvas will be rendered */}
-        </div>
       </main>
     </div>
   );
